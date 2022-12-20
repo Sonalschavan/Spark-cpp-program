5e0bdf17af50a636417b93d0cfb72685fb54ac92
Sonalschavan <121081903+Sonalschavan@users.noreply.github.com>
2022-12-20 12:05:53 +0530

    Update Spark.c

--- Spark.c
+++ Spark.c
@@ -1,5 +1,15 @@
 #include<stdio.h>
+void add();
 main()
 {
 printf("\n Welcome")
+  printf("\n Spark institute")
+  add();
 }
+void add()
+{
+  int a,b,c;
+  printf("\enter a&b");
+  scanf("%d,%d",&a,&b);
+  c=a+b;
+  
